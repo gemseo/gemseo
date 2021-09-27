692177df1335dd551286160299c6e9595d8f321d
Matthias De Lozzo <matthias.delozzo@irt-saintexupery.com>
2021-09-27 18:44:33 +0000

    Check jacobian for given input and output indices and using files

--- tests/utils/test_derivatives_approx.py
+++ tests/utils/test_derivatives_approx.py
@@ -25,13 +25,15 @@ from copy import deepcopy
 from math import cos, exp, log10, sin
 
 import pytest
-from numpy import array, zeros
+from numpy import array, complex128, float64, zeros
 from numpy.linalg import norm
 from scipy.optimize import rosen, rosen_der
 
 from gemseo.api import create_discipline
 from gemseo.core.analytic_discipline import AnalyticDiscipline
+from gemseo.core.discipline import MDODiscipline
 from gemseo.problems.sobieski.wrappers import SobieskiMission
+from gemseo.utils.derivatives.gradient_approximator import GradientApproximationFactory
 from gemseo.utils.derivatives_approx import (
     ComplexStep,
     DisciplineJacApprox,
@@ -161,3 +163,119 @@ def test_opt_step():
                 full_error = trunc_error + cancel_error
                 rel_erro_on_err = abs(log10(abs(full_error)) - log10(abs(err))) < 5
                 assert rel_erro_on_err
+
+
+@pytest.mark.parametrize(
+    "indices,expected_sequence,expected_variables_indices",
+    [
+        ({"y": None}, [0, 1, 2, 3, 4], {"x": [0, 1], "y": [0, 1, 2]}),
+        ({"y": Ellipsis}, [0, 1, 2, 3, 4], {"x": [0, 1], "y": [0, 1, 2]}),
+        ({"y": 1}, [0, 1, 3], {"x": [0, 1], "y": [1]}),
+        ({"y": [2, 4]}, [0, 1, 4, 6], {"x": [0, 1], "y": [2, 4]}),
+        ({"y": slice(0, 2)}, [0, 1, 2, 3], {"x": [0, 1], "y": [0, 1]}),
+        ({}, [0, 1, 2, 3, 4], {"x": [0, 1], "y": [0, 1, 2]}),
+    ],
+)
+def test_compute_io_indices(indices, expected_sequence, expected_variables_indices):
+    """Check that input and output indices are correctly computed from indices."""
+    (
+        indices_sequence,
+        variables_indices,
+    ) = DisciplineJacApprox._compute_variables_indices(
+        indices, ["x", "y"], {"y": 3, "x": 2}
+    )
+    assert indices_sequence == expected_sequence
+    assert variables_indices == expected_variables_indices
+
+
+def test_load_and_dump(tmp_wd):
+    """Check the loading and dumping of a reference Jacobian."""
+    discipline = AnalyticDiscipline("name", {"y": "x", "z": "x"})
+    discipline.execute()
+    apprx = DisciplineJacApprox(discipline)
+    apprx.compute_approx_jac(["z"], ["x"])
+    discipline.linearize()
+    discipline.jac["z"]["x"] = array([[2.0]])
+    file_name = "reference_jacobian.pkl"
+    assert not apprx.check_jacobian(
+        discipline.jac,
+        ["z"],
+        ["x"],
+        discipline,
+        reference_jacobian_path=tmp_wd / file_name,
+        save_reference_jacobian=True,
+    )
+
+    assert not apprx.check_jacobian(
+        discipline.jac,
+        ["z"],
+        ["x"],
+        discipline,
+        reference_jacobian_path=tmp_wd / file_name,
+    )
+
+
+class ToyDiscipline(MDODiscipline):
+    def __init__(self, dtype=float64):
+        super(ToyDiscipline, self).__init__()
+        self.input_grammar.initialize_from_data_names(["x1", "x2"])
+        self.output_grammar.initialize_from_data_names(["y1", "y2"])
+        self.default_inputs = {
+            "x1": array([1.0], dtype=dtype),
+            "x2": array([1.0, 1.0], dtype=dtype),
+        }
+        self.dtype = dtype
+
+    def _run(self):
+        self.local_data["y1"] = self.local_data["x1"] + 2 * self.local_data["x2"][0]
+        self.local_data["y2"] = array(
+            [
+                self.local_data["x1"][0]
+                + 2 * self.local_data["x2"][0]
+                + 3 * self.local_data["x2"][1],
+                2 * self.local_data["x1"][0]
+                + 4 * self.local_data["x2"][0]
+                + 6 * self.local_data["x2"][1],
+            ]
+        )
+
+    def _compute_jacobian(self, inputs=None, outputs=None):
+        self.jac = {
+            "y1": {
+                "x1": array([[1.0]], dtype=self.dtype),
+                "x2": array([[2.0, 0.0]], dtype=self.dtype),
+            },
+            "y2": {
+                "x1": array([[1.0], [2.0]], dtype=self.dtype),
+                "x2": array([[2.0, 3.0], [4.0, 6.0]], dtype=self.dtype),
+            },
+        }
+
+
+@pytest.mark.parametrize("inputs", [["x1"], ["x2"], ["x1", "x2"]])
+@pytest.mark.parametrize("outputs", [["y1"], ["y2"], ["y1", "y2"]])
+@pytest.mark.parametrize("indices", [None, {"x1": 0}, {"y2": 1}, {"x1": 0, "y2": 1}])
+@pytest.mark.parametrize("dtype", [float64, complex128])
+def test_indices(inputs, outputs, indices, dtype):
+    discipline = ToyDiscipline(dtype=dtype)
+    discipline.linearize(force_all=True)
+    apprx = DisciplineJacApprox(discipline)
+    assert apprx.check_jacobian(
+        discipline.jac, outputs, inputs, discipline, indices=indices
+    )
+
+
+def test_factory():
+    factory = GradientApproximationFactory()
+    assert "ComplexStep" in factory.gradient_approximators
+    assert factory.is_available("ComplexStep")
+
+    def function(x):
+        return 2 * x
+
+    assert isinstance(factory.create("FirstOrderFD", function), FirstOrderFD)
+    assert isinstance(factory.create("finite_differences", function), FirstOrderFD)
+    assert isinstance(
+        factory.create("finite_differences", function, step=1e-3), FirstOrderFD
+    )
+    assert isinstance(factory.create("complex_step", function), ComplexStep)
