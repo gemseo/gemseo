8f4a1b39cd5a8b5342d28817482712141e453a77
Alexandre Scotto Di Perrotolo <alexandre.scotto@irt-saintexupery.com>
2023-05-29 07:45:50 +0000

    Feature: handle sparse matrices in the gemseo.utils.comparisons module

--- src/gemseo/utils/comparisons.py
+++ src/gemseo/utils/comparisons.py
@@ -19,48 +19,75 @@
 from __future__ import annotations
 
 from typing import Mapping
+from typing import Union
 
-from numpy import array_equal
+from numpy import asarray
 from numpy import ndarray
 from numpy.linalg import norm
+from scipy.sparse import spmatrix
+from scipy.sparse.linalg import norm as spnorm
 
 from gemseo.utils.data_conversion import flatten_nested_dict
 
+ArrayLike = Union[ndarray, spmatrix]
+
 
 def compare_dict_of_arrays(
-    dict_of_arrays: Mapping[str, ndarray] | Mapping[str, Mapping[str, ndarray]],
-    other_dict_of_arrays: Mapping[str, ndarray] | Mapping[str, Mapping[str, ndarray]],
+    dict_of_arrays: Mapping[str, ArrayLike] | Mapping[str, Mapping[str, ArrayLike]],
+    other_dict_of_arrays: Mapping[str, ArrayLike]
+    | Mapping[str, Mapping[str, ArrayLike]],
     tolerance: float = 0.0,
 ) -> bool:
-    """Check if two dictionaries of NumPy arrays are equal.
+    """Check if two dictionaries of NumPy arrays and/or SciPy sparse matrices are equal.
 
     These dictionaries can be nested.
 
     Args:
-        dict_of_arrays: A dictionary of NumPy arrays.
-        other_dict_of_arrays: Another dictionary of NumPy arrays.
-        tolerance: A relative tolerance.
-            The dictionaries are approximately equal
-            if for any key ``reference_name`` of ``reference_dict_of_arrays``,
-            ``norm(dict_of_arrays[name]-reference_dict_of_arrays[name])
-            /(1+norm(reference_dict_of_arrays))<= cache_tol``
+        dict_of_arrays: A dictionary of NumPy arrays and/or SciPy sparse matrices.
+        other_dict_of_arrays: Another dictionary of NumPy arrays and/or SciPy sparse
+            matrices.
+        tolerance: A relative tolerance. The dictionaries are considered equal if for
+            any key ``reference_name`` of ``reference_dict_of_arrays``,
+            ``norm(dict_of_arrays[name] - reference_dict_of_arrays[name])
+            /(1 + norm(reference_dict_of_arrays)) <= tolerance``.
 
     Returns:
         Whether the dictionaries are equal.
     """
+    # Flatten the dictionnaries if nested
     if any(isinstance(value, Mapping) for value in dict_of_arrays.values()):
         dict_of_arrays = flatten_nested_dict(dict_of_arrays)
         other_dict_of_arrays = flatten_nested_dict(other_dict_of_arrays)
 
-    for key, value in other_dict_of_arrays.items():
-        if key not in dict_of_arrays:
-            return False
+    # Check the keys
+    if dict_of_arrays.keys() != other_dict_of_arrays.keys():
+        return False
 
-        if tolerance:
-            if norm(dict_of_arrays[key] - value) > tolerance * (1.0 + norm(value)):
+    # Check the values
+    if tolerance:
+        for key, value in dict_of_arrays.items():
+            difference = other_dict_of_arrays[key] - value
+
+            if isinstance(difference, spmatrix):
+                norm_diff = spnorm(difference)
+            else:
+                norm_diff = norm(difference)
+
+            if isinstance(value, spmatrix):
+                norm_ref = spnorm(value)
+            else:
+                norm_ref = norm(value)
+
+            if norm_diff > tolerance * (1.0 + norm_ref):
                 return False
-        else:
-            if not array_equal(dict_of_arrays[key], value):
+    else:
+        for key, value in dict_of_arrays.items():
+            is_different = other_dict_of_arrays[key] != value
+
+            if isinstance(is_different, spmatrix):
+                is_different = is_different.data
+
+            if asarray(is_different).any():
                 return False
 
     return True
