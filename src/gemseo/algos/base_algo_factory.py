9443de564563d59564ea752f13809b5109cadcb3
Matthias De Lozzo <matthias.delozzo@irt-saintexupery.com>
2023-12-14 10:12:59 +0000

    feat(BaseAlgoFactory): initialize the grammar of options when creating an algorithm

--- src/gemseo/algos/base_algo_factory.py
+++ src/gemseo/algos/base_algo_factory.py
@@ -29,6 +29,7 @@ from typing import Any
 from typing import ClassVar
 
 from gemseo.core.base_factory import BaseFactory
+from gemseo.utils.string_tools import pretty_str
 
 if TYPE_CHECKING:
     from gemseo.algos.base_problem import BaseProblem
@@ -161,10 +162,9 @@ class BaseAlgoFactory(metaclass=_AlgoFactoryMeta):
         lib_name = self.__algo_name_to_lib_name.get(name)
         if lib_name is None:
             if name not in self.libraries:
-                algorithms = ", ".join(sorted(self.algorithms))
                 raise ImportError(
                     f"No algorithm or library of algorithms named '{name}' is available"
-                    f" ; available algorithms are {algorithms}."
+                    f"; available algorithms are {pretty_str(self.algorithms)}."
                 )
             lib_name = name
             algo_name = ""
@@ -178,6 +178,7 @@ class BaseAlgoFactory(metaclass=_AlgoFactoryMeta):
             obj = self._factory.create(lib_name)
         if algo_name:
             obj.algo_name = name
+            obj.init_options_grammar(algo_name)
         return obj
 
     def execute(
