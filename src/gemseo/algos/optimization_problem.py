da36fd0c0aa57ddf1b863cf40d5d730103004841
Matthias De Lozzo <matthias.delozzo@IRT-AESE.local>
2024-11-26 18:24:19 +0100

    fix(OptimizationProblem): correct logged sentence

--- src/gemseo/algos/optimization_problem.py
+++ src/gemseo/algos/optimization_problem.py
@@ -658,11 +658,12 @@ class OptimizationProblem(EvaluationProblem):
                 in which the optimization problem should be exported.
                 If empty, the root node is considered.
         """
-        LOGGER.info(
-            "Exporting the optimization problem to the file %s at node %s",
-            file_path,
-            hdf_node_path,
-        )
+        msg = "Exporting the optimization problem to the file %s"
+        if hdf_node_path:
+            msg += " at node %s"
+            LOGGER.info(msg, file_path, hdf_node_path)
+        else:
+            LOGGER.info(msg, file_path)
 
         with h5py.File(file_path, "a" if append else "w") as h5file:
             if hdf_node_path:
@@ -722,11 +723,12 @@ class OptimizationProblem(EvaluationProblem):
         Returns:
             The read optimization problem.
         """
-        LOGGER.info(
-            "Importing the optimization problem from the file %s at node %s",
-            file_path,
-            hdf_node_path,
-        )
+        msg = "Importing the optimization problem from the file %s"
+        if hdf_node_path:
+            msg += " at node %s"
+            LOGGER.info(msg, file_path, hdf_node_path)
+        else:
+            LOGGER.info(msg, file_path)
 
         database = Database.from_hdf(file_path, hdf_node_path=hdf_node_path, log=False)
         design_space = database.input_space
