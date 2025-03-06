4edeceeaefaa74c91d49cd6daddf8ccb8341e467
Antoine Dechaume <7801279-AntoineD@users.noreply.gitlab.com>
2025-03-06 21:55:54 +0100

    Fix example

--- tests/test_doc_examples.py
+++ tests/test_doc_examples.py
@@ -40,3 +40,12 @@ def test_script_execution(example_path: Path, tmp_wd: Path, monkeypatch) -> None
     copytree(example_path.parent, dir_path)
     monkeypatch.chdir(dir_path)
     runpy.run_path(example_path.name)
+
+    if example_path.name == "data_converter.py":
+        # Workaround a side effect of an example that globally change a data converter,
+        # with side effects on other examples.
+        # We reset the data converter to its original value.
+        from gemseo.core.data_converters.json import JSONGrammarDataConverter
+        from gemseo.core.grammars.json_grammar import JSONGrammar
+
+        JSONGrammar.DATA_CONVERTER_CLASS = JSONGrammarDataConverter
