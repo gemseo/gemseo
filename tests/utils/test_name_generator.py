d0e4d86c04b231a849e369f5ad8fa285ecfcef9f
Loïc Cousin <loic.cousin@irt-saintexupery.com>
2025-07-29 10:20:37 +0000

    New naming method: Dated UUID

--- tests/utils/test_name_generator.py
+++ tests/utils/test_name_generator.py
@@ -18,6 +18,7 @@
 #    OTHER AUTHORS   - MACROSCOPIC CHANGES
 from __future__ import annotations
 
+from datetime import datetime
 from re import match
 
 import pytest
@@ -34,6 +35,7 @@ from gemseo.utils.name_generator import NameGenerator
     [
         BaseNameGenerator.Naming.UUID,
         BaseNameGenerator.Naming.NUMBERED,
+        BaseNameGenerator.Naming.DATED_UUID,
         "WRONGMETHOD",
     ],
 )
@@ -48,6 +50,14 @@ def test_generate_name(naming) -> None:
     elif naming == BaseNameGenerator.Naming.NUMBERED:
         assert name_1 == "1"
         assert name_2 == "2"
+    elif naming == BaseNameGenerator.Naming.DATED_UUID:
+        assert name_1 != name_2
+        elements_1, elements_2 = name_1.split("_"), name_2.split("_")
+        assert elements_1[0] == elements_2[0]
+        assert datetime.strptime(
+            "_".join(elements_1[0:2]), "%Y-%m-%d_%Hh%Mmin%Ss"
+        ) <= datetime.strptime("_".join(elements_2[0:2]), "%Y-%m-%d_%Hh%Mmin%Ss")
+        assert match(r"[0-9a-fA-F]{12}$", elements_1[2]) is not None
     else:
         assert name_1 is None
         assert name_2 is None
